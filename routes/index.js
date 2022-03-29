22917e504b27cd64647736f98fe0d0c04b695a9b
kmkinney <65429603+kmkinney@users.noreply.github.com>
2022-03-29 16:41:25 -0600

    Finished Activity

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var debug = require('debug')('city:index');
+var fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.sendFile('index.htlm', {root: 'public'})
 });
+
+router.get('/city', (req, res) => {
+    debug('Reading City Data')
+    const prefix = req.query.q
+    debug(`Checking for prefix ${prefix}`)
+    fs.readFile(`${__dirname}/citydata.txt`, (err, data) => {
+        if(err) {
+            debug(err)
+            return res.status(500).send(err)
+        }
+        let matches = []
+        let cities = data.toString().split('\n')
+        for(city of cities){
+            // debug(city)
+            if(city.toString().match(`^${prefix}`)){
+                matches.push({city: city})
+            }
+        }
+        res.status(200).send(JSON.stringify(matches))
+    })
+})
 
 module.exports = router;
